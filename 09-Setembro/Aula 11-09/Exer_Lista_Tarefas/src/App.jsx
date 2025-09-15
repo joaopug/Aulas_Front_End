a74e3093b60ab07d9c345f86df073b95b13d49fd
joaopug <41847866+joaopug@users.noreply.github.com>
2025-09-15 20:41:39 -0300

    Sistema de concluir, excluir e salvar as tarefas na memória, esse último incompleto

--- 09-Setembro/Aula 11-09/Exer_Lista_Tarefas/src/App.jsx
+++ 09-Setembro/Aula 11-09/Exer_Lista_Tarefas/src/App.jsx
@@ -1,31 +1,59 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import "./App.css";
 
+//Esse cara aqui -> ... quebra um objeto e pega os valores dentro dele, aqueles que você definir
+//Parâmetro "disabled" -> serve para desabilitar um botão conforme um critério
+//
 function App() {
   const [tarefa, setTarefa] = useState("");
   const [listaTarefas, setlistaTarefas] = useState([]);
 
-  const adicionarTarefa = () => {
+  useEffect(() => {
+    setlistaTarefas(JSON.parse(localStorage.getItem("LISTA_TAREFAS")));
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("LISTA_TAREFAS", JSON.stringify(listaTarefas));
+  }, [listaTarefas]);
+
+  const adicionarTarefa = () => {
     if (tarefa.trim() === "") {
-      alert("Informe uma tarefa")
-      return
+      alert("Informe uma tarefa");
+      return;
     }
 
     const novaTarefa = {
-      id : Date.now(),
-      nome : tarefa,
+      id: Date.now(),
+      nome: tarefa,
+      completada: false,
     };
 
     setlistaTarefas([...listaTarefas, novaTarefa]);
+
+    
 
     console.log(listaTarefas);
     setTarefa("");
   };
 
+  const alterarSituacao = (id) => {
+    setlistaTarefas(
+      listaTarefas.map((task) =>
+        task.id == id ? { ...task, completada: !task.completada } : task
+      )
+    );
+  };
+
+  const excluirTarefa = (id) => {
+    setlistaTarefas(listaTarefas.filter((task) => task.id != id));
+  };
+
   return (
     <div className="todo-container">
       <h2>Lista de Tarefas ✅</h2>
+
+      <p>Total tarefas : {listaTarefas.length || "Sem Tarefas"}</p>
+
       <div className="input-container">
         <input
           type="text"
@@ -38,11 +66,28 @@ function App() {
 
       <ul>
         {listaTarefas.map((task) => (
-          <li key={task.id}>
+          <li
+            key={task.id}
+            style={{
+              textDecoration: task.completada ? "line-through" : "none",
+              color: task.completada ? "gray" : "black",
+            }}
+          >
             {task.nome}
             <div>
-              <button className="complete-btn">✔</button>
-              <button className="delete-btn">❌</button>
+              <button
+                onClick={() => alterarSituacao(task.id)}
+                className="complete-btn"
+              >
+                ✔
+              </button>
+              <button
+                disabled={task.completada}
+                onClick={() => excluirTarefa(task.id)}
+                className="delete-btn"
+              >
+                ❌
+              </button>
             </div>
           </li>
         ))}
