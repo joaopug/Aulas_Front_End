8c4ee607ebdb0b2da5400153a39122f37c70fe17
joaopug <41847866+joaopug@users.noreply.github.com>
2025-10-01 20:23:47 -0300

    Continuando ex1

--- 09-Setembro/Exercicios_GPT/ex1gpt/src/App.jsx
+++ 09-Setembro/Exercicios_GPT/ex1gpt/src/App.jsx
@@ -6,20 +6,47 @@ function App() {
 
   const [tarefa, setTarefa] = useState("");
 
+  const addTarefa = () => {
+    if (tarefa.trim() === "") {
+      return;
+    }
+    
+    const novaTarefa = {
+      id: crypto.randomUUID(),
+      nome: tarefa,
+    };
+
+    setLista([...lista, novaTarefa]);
+
+    setTarefa("");
+  };
+
+  const removeTarefa = (id) => {
+    setLista(lista.filter((item) => item.id != id));
+  };
+
   return (
     <>
-      <div>
+      <div className="sigma">
         <h1>Lista de Tarefas</h1>
         <input
           type="text"
+          value={tarefa}
           placeholder="Insira a tarefa"
           onChange={(e) => setTarefa(e.target.value)}
         />
-        <h2>{tarefa}</h2>
-        <button onClick={() => setLista([...lista, tarefa])}>Adicionar</button>
+        <button onClick={addTarefa}>Adicionar</button>
         <ul>
           {lista.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>
+              {item.nome}
+              <div>
+                <button
+                  className="rmvBtn"
+                  onClick={() => removeTarefa(item.id)}
+                ></button>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
